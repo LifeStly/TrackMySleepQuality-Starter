747225bb1d628ef358514eef0c7d969654afdcb6
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-08-05 09:09:21 +0700

    06.1 6

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
@@ -27,6 +28,22 @@ abstract class SleepDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: SleepDatabase? = null
-        fun getInstance(context: Context): SleepDatabase {}
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
     }
 }
